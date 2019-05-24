180cb2bd886974e28bdef294c6591f3619d3552b
kcheranev <kcheranev@gmail.com>
2019-05-24 23:53:00 +0300

    up circleci

--- src/main/java/test/demo/DemoController.java
+++ src/main/java/test/demo/DemoController.java
@@ -1,12 +1,14 @@
 package test.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
     @GetMapping
+    @RequestMapping("/")
     public String demoTest() {
         return "Hello world";
     }
