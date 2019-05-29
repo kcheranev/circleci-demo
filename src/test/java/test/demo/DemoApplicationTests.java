c68724be412079a24d0917fdac3e903d849ca168
kcheranev <kcheranev@gmail.com>
2019-05-29 15:35:20 +0300

    testcontainers

--- src/test/java/test/demo/DemoApplicationTests.java
+++ src/test/java/test/demo/DemoApplicationTests.java
@@ -1,10 +1,18 @@
 package test.demo;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.GenericContainer;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,6 +21,26 @@ import org.springframework.test.context.junit4.SpringRunner;
         "SPRING_DATA_MONGODB_PORT = 27017"
 })
 public class DemoApplicationTests {
+
+    private static final int MONGO_PORT = 27017;
+
+    @ClassRule
+    public static GenericContainer mongo = new GenericContainer("mongo:3.1.5")
+            .withExposedPorts(MONGO_PORT);
+
+    @Test
+    public void simpleMongoDbTest() {
+        MongoClient mongoClient = new MongoClient(mongo.getContainerIpAddress(), mongo.getMappedPort(MONGO_PORT));
+        MongoDatabase database = mongoClient.getDatabase("test");
+        MongoCollection<Document> collection = database.getCollection("testCollection");
+
+        Document doc = new Document("name", "foo")
+                .append("value", 1);
+        collection.insertOne(doc);
+
+        Document doc2 = collection.find(new Document("name", "foo")).first();
+        assertEquals("A record can be inserted into and retrieved from MongoDB", 1, doc2.get("value"));
+    }
 
     @Test
     public void contextLoads() {
