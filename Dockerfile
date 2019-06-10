92abdeaedbd43d97453e98f8f33d44de489a6a6a
kcheranev <kcheranev@gmail.com>
2019-06-10 14:32:48 +0300

    fast fix

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,4 @@ VOLUME /tmp
 ARG JAR_FILE
 ARG BUILD_DIR
 COPY ${BUILD_DIR}/${JAR_FILE} app.jar
-COPY ${BUILD_DIR}/newrelic.jar /newrelic/newrelic.jar
-COPY ${BUILD_DIR}/newrelic.yml /newrelic/newrelic.yml
-ENTRYPOINT ["java","-javaagent:/newrelic/newrelic.jar","-jar","/app.jar"]
+ENTRYPOINT ["java","-jar","/app.jar"]
---
+++ src/test/java/test/demo/TestApplicationTest.java
@@ -0,0 +1,19 @@
+package test.demo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestApplicationTest {
+
+    @Test
+    public void test() {
+        assertEquals(0, 0);
+    }
+
+}
